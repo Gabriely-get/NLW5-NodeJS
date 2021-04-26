af1d4003af35e7dfe04acfe4a6dfbedeb8df4a54
Gabriely Willian <Gabrielysantos.informatica@gmail.com>
2021-04-25 23:51:05 -0300

    Altera rota e entidades dos repositories

--- src/repositories/SettingsRepository.ts
+++ src/repositories/SettingsRepository.ts
@@ -1,7 +1,7 @@
 import { Repository, EntityRepository } from "typeorm";
 import { Setting } from "../entities/Setting";
 
-@EntityRepository('settings')
+@EntityRepository(Setting)
 class SettingsRepository extends Repository<Setting> {
 	// console.log('respository');
 }
