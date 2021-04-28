bd5c621fb9b7cf192b9b7e43e2b001abbf08f5c9
Gabriely Willian <Gabrielysantos.informatica@gmail.com>
2021-04-28 15:32:13 -0300

    Adiciona servers como dois modulos

---
+++ src/http.ts
@@ -0,0 +1,31 @@
+import 'reflect-metadata';
+import express from 'express';
+import { createServer } from "http";
+import { Server, Socket } from "socket.io" 
+import path from "path"; //constroi o caminho de algum arquivo
+
+import './database';
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public"))); //define onde esta os arquivos publicos
+app.set("views", path.join(__dirname, "..", "public")); //o caminho que esta minhas views
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => { //renderiza o arquivo html
+	return response.render("html/client.html");
+});
+
+const http = createServer(app); //criando protocolo http
+const io = new Server(http); //criando protocolo ws
+
+io.on("connection", (socket: Socket) => {
+	console.log("Se conectou", socket.id);
+});
+
+app.use(express.json());
+app.use(routes);
+
+export { http, io };
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,4 @@
-import 'reflect-metadata';
-import express from 'express';
-import { createServer } from "http";
-import { Server, Socket } from "socket.io" 
-import path from "path"; //constroi o caminho de algum arquivo
-
-import './database';
-import { routes } from './routes';
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, "..", "public"))); //define onde esta os arquivos publicos
-app.set("views", path.join(__dirname, "..", "public")); //o caminho que esta minhas views
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-
-app.get("/pages/client", (request, response) => { //renderiza o arquivo html
-	return response.render("html/client.html");
-});
-
-const http = createServer(app); //criando protocolo http
-const io = new Server(http); //criando protocolo ws
-
-io.on("connection", (socket: Socket) => {
-	console.log("Se conectou", socket.id);
-});
-
-app.use(express.json());
-app.use(routes);
+import { http } from "./http";
+import "./websocket/client";
 
 http.listen(3333, () => console.log('Server is running!'));
