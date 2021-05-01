44c39162e4c177e54d5f180bca828cc94fad0cd9
Gabriely Willian <Gabrielysantos.informatica@gmail.com>
2021-04-30 21:21:56 -0300

    Merge messages_development

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "../entities/User";
 
-@EntityRepository('users')
+@EntityRepository(User)
 class UsersRepository extends Repository<User> {
 
 }
