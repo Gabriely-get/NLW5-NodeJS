66e03fea2a846b0b2a51a9242f77b900db089c25
Gabriely Willian <gabrielysantos.informatica@gmail.com>
2021-05-06 21:26:40 -0300

    Adiciona function findByEmail

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -2,15 +2,15 @@ import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersService {
-	async create(email: string) {
+	async findByEmail(email: string) {
 		const usersRepository = getCustomRepository(UsersRepository);
 
 		const userAlreadyExists = await usersRepository.findOne(email);
 
-		if(userAlreadyExists) {
-			return userAlreadyExists;
-		}
+		return userAlreadyExists;
+	}
 
+	async create(email: string) {
 		const user = usersRepository.create({ email });
 
 		await usersRepository.save(user);
