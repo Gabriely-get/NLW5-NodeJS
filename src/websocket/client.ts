22b43968d6bb1a2612448c73a53ed7c05acaa0ff
Gabriely Willian <gabrielysantos.informatica@gmail.com>
2021-05-14 02:14:59 -0300

    Adiciona salva mensagem do usuario com front end

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,15 +1,23 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
+import { MessagesService } from "../services/MessagesService";
+
+interface IParams {
+	text: string;
+	email: string;
+}
 
 io.on("connect", (socket) => {
 
 	const connectionsService = new ConnectionsService();
 	const usersService = new UsersService();
+	const messagesService = new MessagesService();
 
 	socket.on("client_first_access", async (params) => {
 		const socketId = socket.id;
-		const { text, email } = params;
+		const { text, email } = params as IParams;
+		let user_id = null;
 
 		const userExists = await usersService.findByEmail(email);
 
@@ -20,8 +28,11 @@ io.on("connect", (socket) => {
 				socket_id: socketId,
 				user_id: user.id
 			});
+
+			user_id = user.id;
 		} else {
 			const connection = await connectionsService.findUserById(userExists.id);
+			user_id = userExists.id;
 
 			if(!connection) {
 
@@ -36,6 +47,11 @@ io.on("connect", (socket) => {
 				await connectionsService.create(connection);
 			}
 		}
+
+		await messagesService.create({
+			text,
+			user_id
+		});
 
 		// Salvar a conexao do usuario com o socket_id &
 		// Salvar a mesma conexao desse mesmo user com socket_id diferente
