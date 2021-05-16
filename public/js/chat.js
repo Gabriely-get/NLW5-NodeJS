a9b68badaf4ef04d6c7e8cdef8d08ced1972877c
Gabriely Willian <gabrielysantos.informatica@gmail.com>
2021-05-16 17:47:26 -0300

    Feature: Carrega mensagem do admin no chat do usuario

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+let socket_admin_id = null;
+
 document.querySelector('#start_chat').addEventListener("click", (event) => {
   const socket = io();
 
@@ -48,8 +50,19 @@ document.querySelector('#start_chat').addEventListener("click", (event) => {
             message_admin: message.text
           });
 
-          document.getElementById("messages_admin").innerHTML += rendered;
+          document.getElementById("messages").innerHTML += rendered;
       }
     });
+  });
+
+  socket.on("admin_send_to_client", message => {
+    socket_admin_id = message.socket_id;
+    var template_admin = document.getElementById("admin-template").innerHTML;
+
+    const rendered = Mustache.render(template_admin, {
+      message_admin: message.text
+    });
+
+    document.getElementById("messages").innerHTML += rendered;
   });
 });
